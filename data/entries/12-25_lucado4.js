e8e7ac9ea76ceb5c5b4e2068267515a762093bd0
le-lien <le.dunglien@gmail.com>
2025-12-21 16:56:26 +0100

    25 Dec entered.

--- data/entries/12-25_lucado4.js
+++ data/entries/12-25_lucado4.js
@@ -6,9 +6,9 @@
       "ref": 'PHILIPPIANS 2:7',
       "text": 'He gave up his place with God and made himself nothing. He was born to be a man and became like a servant.'
     },
-    "body": 'On a starry night, more than two thousand years ago, Jesus was born. It all happened in a most amazing moment … a moment like no other.\nThe Holy One became a human. He stepped out of the majesty of heaven and into the arms of a humble peasant girl.\nThe All-Powerful One, in an instant, became flesh and blood. The One who was larger than the universe became the tiniest of infants. And He who created the world with a word chose to depend upon a young girl named Mary for His human life.\nGod had come down from heaven. God had come near to His people …\nto show us the way back home to heaven. ',
+    "body": 'On a starry night, more than two thousand years ago, Jesus was born. It all happened in a most amazing moment ... a moment like no other.\nThe Holy One became a human. He stepped out of the majesty of heaven and into the arms of a humble peasant girl.\nThe All-Powerful One, in an instant, became flesh and blood. The One who was larger than the universe became the tiniest of infants. And He who created the world with a word chose to depend upon a young girl named Mary for His human life.\nGod had come down from heaven. God had come near to His people ... to show us the way back home to heaven. ',
     "dailyPrayer": {
       "title": 'Growing in Grace',
-      "text": 'Take some time today to read Luke 1:26–38 and Luke 2:1–20, the story of how Jesus came to earth. Then read John 3:1–21, the story of why Jesus came to earth. This is what Christmas is all about.'
+      "text": 'Take some time today to read <a href="https://www.bible.com/bible/114/LUK.1.NKJV?parallel=151">Luke 1:26–38</a> and <a href="https://www.bible.com/bible/114/LUK.2.NKJV">Luke 2:1–20</a>, the story of how Jesus came to earth. Then read <a href="https://www.bible.com/bible/114/JHN.3.NKJV?parallel=151">John 3:1–21</a>, the story of why Jesus came to earth. This is what Christmas is all about.'
     }
 }
--- data/lucado1.js
+++ data/lucado1.js
@@ -5403,7 +5403,7 @@
       "ref": 'PHILIPPIANS 2:7',
       "text": 'He gave up his place with God and made himself nothing. He was born to be a man and became like a servant.'
     },
-    "body": 'It all happened in a most remarkable moment … a moment like no other… .\nGod became a man. Divinity arrived. Heaven opened herself and placed her most precious one in a human womb.\nThe omnipotent, in one instant, became flesh and blood. The one who was larger than the universe became a microscopic embryo. And he who sustains the world with a word chose to be dependent upon the nourishment of a young girl.\nGod had come near. ',
+    "body": 'It all happened in a most remarkable moment ... a moment like no other... .\nGod became a man. Divinity arrived. Heaven opened herself and placed her most precious one in a human womb.\nThe omnipotent, in one instant, became flesh and blood. The one who was larger than the universe became a microscopic embryo. And he who sustains the world with a word chose to be dependent upon the nourishment of a young girl.\nGod had come near.',
     "dailyPrayer": {
       "title": '',
       "text": ''
