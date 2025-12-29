207093ed1310dbbe7e89eed85a9cd7891d57de01
le-lien <le.dunglien@gmail.com>
2025-12-29 18:30:20 +0100

    1 Jan entered

--- data/lucado1.js
+++ data/lucado1.js
@@ -6,7 +6,7 @@
       "ref": 'PSALM 142:1',
       "text": 'I cry out to the LORD; I pray to the LORD for mercy.'
     },
-    "body": 'You can talk to God because God listens. Your voice matters in heaven. He takes you very seriously. When you enter his presence, he turns to you to hear your voice. No need to fear that you will be ignored. Even if you stammer or stumble, even if what you have to say impresses no one, it impresses God, and he listens. He listens to the painful plea of the elderly in the rest home. He listens to the gruff confession of the death-row inmate.\nWhen the alcoholic begs for mercy, when the spouse seeks guidance, when the businessman steps off the street into the chapel, God listens.\nIntently. Carefully. ',
+    "body": 'You can talk to God because God listens. Your voice matters in heaven. He takes you very seriously. When you enter his presence, he turns to you to hear your voice. No need to fear that you will be ignored. Even if you stammer or stumble, even if what you have to say impresses no one, it impresses God, and he listens. He listens to the painful plea of the elderly in the rest home. He listens to the gruff confession of the death-row inmate. When the alcoholic begs for mercy, when the spouse seeks guidance, when the businessman steps off the street into the chapel, God listens.\nIntently. Carefully. ',
     "dailyPrayer": {
       "title": '',
       "text": ''
--- data/lucado2.js
+++ data/lucado2.js
@@ -6,7 +6,7 @@
       "ref": 'EXODUS 35:35 NKJV',
       "text": 'He has filled them with skill.'
     },
-    "body": 'You were born prepacked. God looked at your entire life, determined your assignment, and gave you the tools to do the job.\nBefore traveling, you do something similar. You consider the demands of the journey and pack accordingly. Cold weather? Bring a jacket. Business meeting? Carry the laptop. Time with grandchildren? Better take some sneakers and pain medication.\nGod did the same with you. Joe will research animals . . . install curiosity. Meagan will lead a private school . . . an extra dose of management. I need Eric to comfort the sick … include a healthy share of compassion. Denalyn will marry Max . . . instill a double portion of patience.\nGod packed you on purpose for a purpose. ',
+    "body": 'You were born prepacked. God looked at your entire life, determined your assignment, and gave you the tools to do the job.\nBefore traveling, you do something similar. You consider the demands of the journey and pack accordingly. Cold weather? Bring a jacket. Business meeting? Carry the laptop. Time with grandchildren? Better take some sneakers and pain medication.\nGod did the same with you. <i>Joe will research animals ... install curiosity. Meagan will lead a private school ... an extra dose of management. I need Eric to comfort the sick ... include a healthy share of compassion. Denalyn will marry Max ... instill a double portion of patience.</i>\nGod packed you on purpose for a purpose. ',
     "dailyPrayer": {
       "title": '',
       "text": ''
