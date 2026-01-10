5cf1b5ef01809e9ec7bf55aae53e59d1db5cbbe8
le-lien <le.dunglien@gmail.com>
2026-01-10 07:08:09 +0100

    12 Jan entered.

--- data/entries/01-12_lucado2.js
+++ data/entries/01-12_lucado2.js
@@ -6,7 +6,7 @@
       "ref": 'MATTHEW 21:24',
       "text": '“If you believe, you will get anything you ask for in prayer.”'
     },
-    "body": '“If you believe, you will get anything you ask for in prayer.”\nDon’t reduce this grand statement to the category of new cars and paychecks. Don’t limit the promise of this passage to the selfish pool of perks and favors. The fruit God assures is far greater than earthly wealth.\nHis dreams are much greater than promotions and proposals.\nGod wants you to fly. He wants you to fly free of yesterday’s guilt. He wants you to fly free of today’s fears. He wants you to fly free of tomorrow’s grave. Sin, fear, and death. These are the mountains he has moved. These are the prayers he will answer. That is the fruit he will grant.\nThis is what he longs to do. ',
+    "body": '<i>“If you believe, you will get anything you ask for in prayer.”</i>\nDon’t reduce this grand statement to the category of new cars and paychecks. Don’t limit the promise of this passage to the selfish pool of perks and favors. The fruit God assures is far greater than earthly wealth. His dreams are much greater than promotions and proposals.\nGod wants you to fly. He wants you to fly free of yesterday’s guilt. He wants you to fly free of today’s fears. He wants you to fly free of tomorrow’s grave. Sin, fear, and death. These are the mountains he has moved. These are the prayers he will answer. That is the fruit he will grant. This is what he longs to do. ',
     "dailyPrayer": {
       "title": '',
       "text": ''
--- data/lucado1.js
+++ data/lucado1.js
@@ -169,9 +169,9 @@
     "passage": 'He Still Moves Stones',
     "dailyVerse": {
       "ref": 'LUKE 10:40–42 NKJV',
-      "text": 'Martha was distracted with much serving… . But Mary has chosen that good part, which will not be taken away from her.'
+      "text": 'Martha was distracted with much serving... But Mary has chosen that good part, which will not be taken away from her.'
     },
-    "body": 'Martha is worried about something good. She’s having Jesus over for dinner. She’s literally serving God. Her aim was to please Jesus. But she made a common, yet dangerous mistake. As she began to work for him, her work became more important than her Lord. What began as a way to serve Jesus, slowly and subtly became a way to serve self… . She has forgotten that the meal is to honor Jesus, not Martha… .\nIt’s easy to forget who is the servant and who is to be served. ',
+    "body": 'Martha is worried about something good. She’s having Jesus over for dinner. She’s literally serving God. Her aim was to please Jesus. But she made a common, yet dangerous mistake. As she began to work for him, her work became more important than her Lord. What began as a way to serve Jesus, slowly and subtly became a way to serve self... She has forgotten that the meal is to honor Jesus, not Martha...\nIt’s easy to forget who is the servant and who is to be served.',
     "dailyPrayer": {
       "title": '',
       "text": ''
