5f9bdb88814ed846ecf7c327bdafe4b77f6b7953
le-lien <le.dunglien@gmail.com>
2026-03-07 23:27:46 +0100

    edit preferences.

--- add_link.R
+++ add_link.R
@@ -2,21 +2,21 @@
 
 # Your mapping
 book_map <- c(
-  "Genesis"="GEN","Exodus"="EXO","Exod"="EXO","Leviticus"="LEV","Numbers"="NUM","Deuteronomy"="DEU",
+  "Genesis"="GEN","Exodus"="EXO","Exod"="EXO","Leviticus"="LEV","Numbers"="NUM","Deuteronomy"="DEU","Deu"="DEU",
   "Joshua"="JOS","Judges"="JDG","Ruth"="RUT",
   "1 Samuel"="1SA","2 Samuel"="2SA",
   "1 Kings"="1KI","2 Kings"="2KI",
-  "1 Chronicles"="1CH","2 Chronicles"="2CH",
+  "1 Chronicles"="1CH","2 Chronicles"="2CH","2 Chron"="2CH",
   "Ezra"="EZR","Nehemiah"="NEH","Esther"="EST",
-  "Job"="JOB","Psalm"="PSA","Ps"="PSA","Psalms"="PSA","Proverbs"="PRO","Ecclesiastes"="ECC","Song of Songs"="SNG",
-  "Isaiah"="ISA","Isa=ISA","Jeremiah"="JER","Jer"="JER","Lamentations"="LAM","Ezekiel"="EZK","Daniel"="DAN",
+  "Job"="JOB","Psalm"="PSA","Ps"="PSA","Psalms"="PSA","Proverbs"="PRO","Ecclesiastes"="ECC","Song of Solomon"="SNG","Song"="SNG",
+  "Isaiah"="ISA", "Isa"="ISA","Isa=ISA","Jeremiah"="JER","Jer"="JER","Lamentations"="LAM","Ezekiel"="EZK","Daniel"="DAN",
   "Hosea"="HOS","Joel"="JOL","Amos"="AMO","Obadiah"="OBA","Jonah"="JON",
   "Micah"="MIC","Nahum"="NAM","Habakkuk"="HAB","Zephaniah"="ZEP","Haggai"="HAG","Zechariah"="ZEC","Malachi"="MAL",
   "Matthew"="MAT","Matt"="MAT","Mark"="MRK","Luke"="LUK","John"="JHN","Acts"="ACT",
   "Romans"="ROM","Rom"="ROM","1 Cor"="1CO","2 Cor"="2CO","1 Corinthians"="1CO","2 Corinthians"="2CO",
-  "Galatians"="GAL","Gal"="GAL","Ephesians"="EPH","Philippians"="PHP","Phil"="PHP","Colossians"="COL",
+  "Galatians"="GAL","Gal"="GAL","Ephesians"="EPH","Philippians"="PHP","Phil"="PHP","Colossians"="COL","Col"="COL",
   "1 Thessalonians"="1TH", "1 Thess"="1TH","2 Thessalonians"="2TH",
-  "1 Timothy"="1TI","2 Timothy"="2TI","Titus"="TIT","Philemon"="PHM","HEBREWS"="HEB",
+  "1 Timothy"="1TI","2 Timothy"="2TI","2 Tim"="2TI","Titus"="TIT","Philemon"="PHM","HEBREWS"="HEB",
   "Hebrews"="HEB","Heb"="HEB","James"="JAS","1 Peter"="1PE","2 Peter"="2PE",
   "1 John"="1JN","2 John"="2JN","3 John"="3JN","Jude"="JUD","Revelation"="REV"
 )
