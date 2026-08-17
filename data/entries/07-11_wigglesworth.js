15943824e66aceaa2193c2827ee1e97fe6a03686
le-lien <le.dunglien@gmail.com>
2026-08-17 20:58:00 +0200

    edit 17 August tripp

---
+++ data/entries/07-11_wigglesworth.js
@@ -0,0 +1,14 @@
+  "07-11": {
+    "title": 'The Holy Spirit—Our Comforter',
+    "note": '',
+    "passage": 'Scripture reading: <a href="https://www.bible.com/bible/114/2CO.1.3-11.NKJV?parallel=151">2 Corinthians 1:3–11</a>',
+    "dailyVerse": {
+      "ref": '2 Corinthians 1:4',
+      "text": '[God] comforts us in all our tribulation, that we may be able to comfort those who are in any trouble, with the comfort with which we ourselves are comforted by God.'
+    },
+    "body": 'We need a revelation of a greater power, an abiding presence sustaining and comforting us in the hour of trial, W ready at a moment‘s notice, an inbreathing of God in the human life. What more do we need in these last days when perilous times are upon us than to be filled, saturated, baptized with the Holy Spirit? Baptized. Baptized into Him, never to come out. How comforting! Exhilarating! Joyful! May it please the Lord to establish us in this state of grace. May we know nothing among men except Jesus Christ and Him crucified (1 Cor. 2:2). May we be clothed with His Spirit—nothing outside of the blessed Holy Spirit. This, beloved, is God‘s ideal for us. Are we here in this experience? \n\nWhere He may lead me I will go,\n\nFor I have learned to trust Him so, \n\nAnd I remember it was for me, \n\nThat He was slain on Calvary. \n\nGod has chosen me to go through certain experiences to profit others. In all ages, God has had His witnesses, and He is teaching, chastening, correcting, and moving me just up to the point that I am able to bear it, in order to meet a needy soul who would otherwise go down without such comfort. All the chastening and the hardship is because we are able to bear it. No, we are not able, but we yield to Another—even the Holy Spirit. We are strengthened so that we may endure and so that we may comfort others “with the comfort with which we ourselves are comforted by God.” \n\nWhy do we need brokenness and travail? The reason can be found in the book of Psalms: “Before I was afflicted I went astray, but now I keep Your word” (Ps. 119:67).',
+    "dailyPrayer": {
+      "title": 'Thought for today',
+      "text": 'The God in you will not fail if you believe the Word of God.'
+    }
+}
